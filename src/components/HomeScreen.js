169c80bae1263cea675daeec0dcfbb9799a790e0
SinclairHudson <hudson.sinclair00@gmail.com>
2019-06-22 13:14:35 -0400

    Fixed GPS Calculations

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -9,8 +9,6 @@ class HomeScreen extends React.Component {
         super(props);
         this.requestLocationPermission();
         this.state = {
-            radius: 20,
-            enabled: true,
             longitude: 0,
             latitude: 0,
         };
@@ -66,7 +64,7 @@ class HomeScreen extends React.Component {
         );
         this.watchId = navigator.geolocation.watchPosition(
             (position) => {
-                this.setState(position);
+                this.setState(position.coords);
                 this.checkMatches();
             },
             (error) => this.setState({error: error.message}),
